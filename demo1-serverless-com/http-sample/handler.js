7ab88f9b3a71a94b8071ba0450ce239493bbea92
Tobias Meixner - Asus <meixner.tobias@gmail.com>
2017-09-23 09:59:23 +0700

    added hello-world 2

--- demo1-serverless-com/http-sample/handler.js
+++ demo1-serverless-com/http-sample/handler.js
@@ -4,7 +4,7 @@ module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello Stamford, the current time is ${new Date().toTimeString()}.`,
+      message: `Hello Stamford 2, the current time is ${new Date().toTimeString()}.`,
     }),
   };
 
